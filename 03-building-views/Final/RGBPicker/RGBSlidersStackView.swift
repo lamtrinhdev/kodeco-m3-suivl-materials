b948814ecb913fba55fbf6f885bef2d9f9bf6dfe
Renan Dias <renanbenattidias@gmail.com>
2024-02-06 17:48:00 -0400

    Added Starter and Final sample projects for lesson 1, 2 and 3.

---
+++ 03-building-views/Final/RGBPicker/RGBSlidersStackView.swift
@@ -0,0 +1,67 @@
+/// Copyright (c) 2024 Kodeco Inc.
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import SwiftUI
+
+struct RGBSlidersStackView: View {
+  @Binding var color: Color
+  @Binding var red: Double
+  @Binding var green: Double
+  @Binding var blue: Double
+
+  var body: some View {
+    VStack {
+      RGBSliderView(rgbSlider: .red, value: $red)
+      RGBSliderView(rgbSlider: .green, value: $green)
+      RGBSliderView(rgbSlider: .blue, value: $blue)
+
+      Button("Set Color") {
+        color = Color(
+            red: red/255,
+            green: green/255,
+            blue: blue/255
+        )
+      }
+      .modifier(.primaryButton)
+    }
+  }
+}
+
+#Preview {
+  RGBSlidersStackView(
+    color: .constant(.red),
+    red: .constant(50),
+    green: .constant(50),
+    blue: .constant(50)
+  )
+  .padding()
+}
